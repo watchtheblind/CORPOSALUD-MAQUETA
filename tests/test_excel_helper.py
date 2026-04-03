72f15b87175cc84b7a9897e9a8308f621ce471fe
luis <ljmm2k2@gmail.com>
2026-04-03 08:27:16 -0400

    test: create data saving test -Create dummy data for saving into Excel file -Create Excel file -Delete Excel file once test is over

---
+++ tests/test_excel_helper.py
@@ -0,0 +1,27 @@
+import unittest
+import os
+from src.models.salary_record import WorkerRecord
+from src.utils.excel_helper import ExcelHelper
+
+class TestExcelHelper(unittest.TestCase):
+    def setUp(self):
+        """Set up a dummy record with the corporation structure"""
+        self.test_file = "tests/test_output.xlsx"
+        self.dummy_record = WorkerRecord(
+            employee_id="V22333444",
+            full_names="LUIS MONASTERIOS",
+            entry_date="15/10/2023",
+            base_salary=2500.00,
+            working_place="CENTRO CARDIOLOGICO MARACAY",
+            job_charge="SYSTEMS ENGINEER",
+            source_file="maqueta_abril.pdf"
+        )
+    def test_save_individual_report(self):
+        """verifies Excel creation with the new file structure"""
+        ExcelHelper.save_individual_report(self.dummy_record, self.test_file)
+        self.assertTrue(os.path.exists(self.test_file))
+    def tearDown(self):
+        """clean up generated files after each test"""
+        if os.path.exists(self.test_file): os.remove(self.test_file)
+if __name__ == '__main__':
+    unittest.main()
