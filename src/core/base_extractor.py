981d19fbd7b8982ec4a4f72120bd82dc3843f43e
luis <ljmm2k2@gmail.com>
2026-04-03 10:26:53 -0400

    chore: improve BaseExtractor class and methods Description

--- src/core/base_extractor.py
+++ src/core/base_extractor.py
@@ -3,8 +3,17 @@ from typing import List
 from src.models.worker_record import WorkerRecord
 
 class BaseExtractor(ABC):
-    """Any OCR engine must implement this method"""
+    """
+    Abstract Base Class for all extraction engines.
+    Ensures that any extractor (Tesseract, AWS, Google)
+    returns a list of WorkerRecord objects.
+    Any OCR engine must implement this method
+    """
     @abstractmethod
     def extract_data(self, file_path: str) -> List[WorkerRecord]:
-        """extracts information from file and maps it to the model"""
+        """
+        Reads a file and returns a list of processed WorkerRecords.
+        :param file_path: Path to the PDF or Image file.
+        :return: List of WorkerRecord entities.
+        """
         pass
