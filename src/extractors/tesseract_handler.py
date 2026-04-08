27d8fe857724022c42a4b7865893c28865a5bf2a
luis <ljmm2k2@gmail.com>
2026-04-08 10:09:25 -0400

    WIP: create tesseract_handler.py

--- src/extractors/tesseract_handler.py
+++ src/extractors/tesseract_handler.py
@@ -1,21 +1,20 @@
 import pytesseract
 from pdf2image import convert_from_path
-from PIL import Image
-from typing import List
 from src.core.base_extractor import BaseExtractor
 from src.models.worker_record import WorkerRecord
-from src.utils.config_loader import ConfigLoader
+from src.utils.data_normalizer import DataNormalizer
 
 class TesseractHandler(BaseExtractor):
     """
-        Convert PDF to images and extract text using OCR.
+        Converts PDF to images and extract text using OCR.
     """
-    def __init__(self, config: ConfigLoader):
+    def __init__(self, config, mapping, normalizer: DataNormalizer):
         self.config = config
-        # Configure the Tesseract executable path from JSON
+        self.mapping = mapping
+        self.normalizer = normalizer
         pytesseract.pytesseract.tesseract_cmd = self.config.get("ocr.tesseract_path")
 
-    def extract_data(self, file_path: str) -> List[WorkerRecord]:
+    def extract_data(self, file_path: str) -> list[WorkerRecord]:
         """
             Orchestrates the conversion from PDF to Text.
             Note: For now, we return an empty list or raw objects to focus on the OCR connection.
@@ -23,15 +22,10 @@ class TesseractHandler(BaseExtractor):
         #Convert PDF to PIL images
         # We use a lower DPI (150-200) to save RAM on low-end PCs
         pages = convert_from_path(file_path, 200)
-
-        raw_text = ""
+        all_workers = []
         for page in pages:
-            #you can put some image processing here
-            #Performing OCR
-            raw_text += pytesseract.image_to_string(page, lang=self.config.get("ocr.language"))
-
-        #MOMENTARILY, we print the text to make sure it works
-        print(f"--- Raw OCR Output for {file_path} ---")
-        print(raw_text[:500])  # First 500 chars
-
-        return []  # We will implement the parsing logic in the next step
+            raw_text = pytesseract.image_to_string(page, lang=self.config.get("ocr.lang"))
+            record = self._parse_worker_text(raw_text, file_path)
+            if record:
+                all_workers.append(record)
+        return all_workers
